9a888200381f40e0aefe3d62f917232a7e1945c8
Kennie NG <jundywoo@gmail.com>
2018-12-21 22:27:53 +0800

    https

--- gcp-quiz/src/main/java/com/ken/gcp/quiz/service/content/ContentConstants.java
+++ gcp-quiz/src/main/java/com/ken/gcp/quiz/service/content/ContentConstants.java
@@ -9,6 +9,6 @@ public final class ContentConstants {
 	// friendly constants only inside content package
 	static final String HTML_HEADER = "<!DOCTYPE html><html><head><title>";
 	static final String HTML_HEADER2 = "</title><link rel=\"shortcut icon\" href=\"https://storage.googleapis.com/kennieng-quiz/quiz-icon.ico\"></head><body>";
-	static final String HTML_BODY_END = "<script type=\"text/javascript\" src=\"http://js.users.51.la/19799255.js\"></script></body><html>";
+	static final String HTML_BODY_END = "<script type=\"text/javascript\" src=\"https://js.users.51.la/19799255.js\"></script></body><html>";
 
 }
